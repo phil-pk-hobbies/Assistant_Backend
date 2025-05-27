8df5cd51599bfe475ef2b307033d189439dc6285
phil-pk-hobbies <phil@pk-hobbies.com>
2025-05-26 20:45:29 -0400

    Fix vector store files endpoint to return filenames

--- assistants/tests.py
+++ assistants/tests.py
@@ -518,14 +518,16 @@ class VectorStoreFilesViewTests(TestCase):
         assistant = Assistant.objects.create(name='VS', vector_store_id='vs_1')
 
         list_mock = MagicMock(return_value=types.SimpleNamespace(data=[
-            {'id': 'file1', 'filename': 'foo.txt'}
+            {'id': 'file1', 'file_id': 'src_1'}
         ]))
+        retrieve_mock = MagicMock(return_value=types.SimpleNamespace(filename='foo.txt'))
 
         class DummyClient:
             def __init__(self):
                 self.vector_stores = types.SimpleNamespace(
                     files=types.SimpleNamespace(list=list_mock)
                 )
+                self.files = types.SimpleNamespace(retrieve=retrieve_mock)
 
         dummy_openai = types.SimpleNamespace(OpenAI=lambda api_key=None: DummyClient())
 
@@ -537,6 +539,7 @@ class VectorStoreFilesViewTests(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.json(), [{'id': 'file1', 'filename': 'foo.txt'}])
         list_mock.assert_called_with(vector_store_id='vs_1')
+        retrieve_mock.assert_called_with('src_1')
 
     def test_missing_vector_store_returns_404(self):
         assistant = Assistant.objects.create(name='NoVS')
